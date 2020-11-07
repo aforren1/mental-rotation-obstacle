44dd886de66f09dc9a9a3390208c65e7e8a8b9f7
Alex Forrence <alex.forrence@gmail.com>
2020-11-06 23:26:50 -0500

    fix break-taking calc

--- src/scripts/scenes/mainScene.js
+++ src/scripts/scenes/mainScene.js
@@ -35,7 +35,7 @@ export default class MainScene extends Phaser.Scene {
     let height = this.game.config.height
     let center = height / 2
     this.center = center
-    this.is_intro = source === 'ttle'
+    this.is_intro = source === 'title'
     if (this.is_intro) {
       // shuffle arrays once
       shuffleArray(trials.warmup)
@@ -251,13 +251,15 @@ export default class MainScene extends Phaser.Scene {
                   this.entering = true
                   // check if we should transition/pause?
                   if (this.trial_counter >= this.trial_table.length) {
+                    this.txt_counter.visible = false
                     this.state = states.END_SECTION
                   } else if (
                     this.trial_table.length >= 80 &&
-                    this.trial_counter % Math.round(this.trial_counter / 3) == 0
+                    this.trial_counter % Math.round(this.trial_table.length / 3) == 0
                   ) {
                     this.state = states.TAKE_A_BREAK
                   }
+                  console.log(this.state)
                 })
               })
             })
